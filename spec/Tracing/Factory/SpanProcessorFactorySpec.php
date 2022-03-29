2eac2f061a397e5fd0a956b7353807c01b510ae5
Christian Daguerre <cdaguerre@worldia.com>
2022-03-29 07:56:42 +0200

    add span processor spec

---
+++ spec/Tracing/Factory/SpanProcessorFactorySpec.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the worldia/instrumentation-bundle package.
+ * (c) Worldia <developers@worldia.com>
+ */
+
+namespace spec\Instrumentation\Tracing\Factory;
+
+use Instrumentation\Tracing\Factory\SpanProcessorFactory;
+use OpenTelemetry\SDK\Trace\SpanProcessor\BatchSpanProcessor;
+use OpenTelemetry\SDK\Trace\SpanProcessor\NoopSpanProcessor;
+use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
+use PhpSpec\ObjectBehavior;
+
+class SpanProcessorFactorySpec extends ObjectBehavior
+{
+    public function it_is_initializable(): void
+    {
+        $this->beAnInstanceOf(SpanProcessorFactory::class);
+    }
+
+    public function it_creates_span_processors(): void
+    {
+        $this->create('batch')->shouldReturnAnInstanceOf(BatchSpanProcessor::class);
+        $this->create('simple')->shouldReturnAnInstanceOf(SimpleSpanProcessor::class);
+        $this->create('noop')->shouldReturnAnInstanceOf(NoopSpanProcessor::class);
+        $this->create('none')->shouldReturnAnInstanceOf(NoopSpanProcessor::class);
+    }
+
+    public function it_throws_an_exception_for_unknown_span_processor(): void
+    {
+        $this->shouldThrow(\InvalidArgumentException::class)->during('create', ['some_processor']);
+    }
+}
