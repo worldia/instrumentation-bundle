93a75205b68232d8c4a03e012bfe8a36ac5b88d5
Christian Daguerre <cdaguerre@worldia.com>
2022-03-29 08:03:24 +0200

    clean cs config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,16 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
-/*
- * This file is part of the platform/client-common package.
- * (c) Worldia <developers@worldia.com>
- */
-
-try {
-    require_once 'vendor/autoload.php';
-} catch (\Throwable) {
-    require_once __DIR__ . '/../../vendor/autoload.php';
-}
+require_once 'vendor/autoload.php';
 
 return CodingStandards\Factory::createPhpCsFixerConfig(__DIR__);
