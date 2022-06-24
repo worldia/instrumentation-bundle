61558b90c8e5f3f5fbae10357f4737017f53908c
Camille Dejoye <camilledejoye@gmail.com>
2022-06-24 15:23:11 +0200

    chore: test TraceContextStamp

---
+++ spec/Tracing/Propagation/Messenger/TraceContextStampSpec.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the worldia/instrumentation-bundle package.
+ * (c) Worldia <developers@worldia.com>
+ */
+
+namespace spec\Instrumentation\Tracing\Propagation\Messenger;
+
+use OpenTelemetry\API\Trace\NonRecordingSpan;
+use OpenTelemetry\API\Trace\SpanContext;
+use OpenTelemetry\API\Trace\TraceState;
+use PhpSpec\ObjectBehavior;
+
+class TraceContextStampSpec extends ObjectBehavior
+{
+    public function it_fails_when_there_is_no_parent_trace(): void
+    {
+        $this->shouldThrow(\Throwable::class)->duringInstantiation();
+    }
+
+    public function it_creates_stamp_without_state(): void
+    {
+        $span = new NonRecordingSpan(SpanContext::create(
+            'b23f37322b169de7bcaf63b9f84b1427',
+            '3c17130d40834256',
+        ));
+        $scope = $span->activate();
+
+        $this->getTraceParent()->shouldBe('00-b23f37322b169de7bcaf63b9f84b1427-3c17130d40834256-00');
+        $this->getTraceState()->shouldBe(null);
+
+        $scope->detach();
+        $span->end();
+    }
+
+    public function it_creates_stamp_with_state(): void
+    {
+        $span = new NonRecordingSpan(SpanContext::create(
+            'b23f37322b169de7bcaf63b9f84b1427',
+            '3c17130d40834256',
+            SpanContext::TRACE_FLAG_DEFAULT,
+            (new TraceState())->with('key', 'value'),
+        ));
+        $scope = $span->activate();
+
+        $this->getTraceParent()->shouldBe('00-b23f37322b169de7bcaf63b9f84b1427-3c17130d40834256-00');
+        $this->getTraceState()->shouldBe('key=value');
+
+        $scope->detach();
+        $span->end();
+    }
+}
