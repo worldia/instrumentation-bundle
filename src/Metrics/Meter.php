79395bc3f15b90d1c3cf4027638ca02b04b1ccd9
Julien Ballestracci <jballestracci@worldia.com>
2023-12-19 11:35:39 +0100

    fix phpstan with docs

--- src/Metrics/Meter.php
+++ src/Metrics/Meter.php
@@ -23,31 +23,104 @@ class Meter implements MeterInterface
     ) {
     }
 
+    /**
+     * Creates a `Counter`.
+     *
+     * @param string $name name of the instrument
+     * @param string|null $unit unit of measure
+     * @param string|null $description description of the instrument
+     * @param array<string> $advisory an optional set of recommendations
+     * @return CounterInterface created instrument
+     *
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/metrics/api.md#counter-creation
+     */
     public function createCounter(string $name, string $unit = null, string $description = null, array $advisory = []): CounterInterface
     {
         return new CounterAdapter($name, $description ?: '', $this->collectorRegistry);
     }
 
-    public function createObservableCounter(string $name, string $unit = null, string $description = null, callable ...$callbacks): ObservableCounterInterface
+    /**
+     * Creates an `ObservableCounter`.
+     *
+     * @param string $name name of the instrument
+     * @param string|null $unit unit of measure
+     * @param string|null $description description of the instrument
+     * @param array<string>|callable $advisory an optional set of recommendations, or
+     *        deprecated: the first callback to report measurements
+     * @param callable ...$callbacks responsible for reporting measurements
+     * @return ObservableCounterInterface created instrument
+     *
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/metrics/api.md#asynchronous-counter-creation
+     */
+    public function createObservableCounter(string $name, string $unit = null, string $description = null, $advisory = [], callable ...$callbacks): ObservableCounterInterface
     {
         throw new \LogicException(sprintf('Method %s is not implemented', __METHOD__));
     }
 
+    /**
+     * Creates a `Histogram`.
+     *
+     * @param string $name name of the instrument
+     * @param string|null $unit unit of measure
+     * @param string|null $description description of the instrument
+     * @param array<string> $advisory an optional set of recommendations, e.g.
+     *        <code>['ExplicitBucketBoundaries' => [0.25, 0.5, 1, 5]]</code>
+     * @return HistogramInterface created instrument
+     *
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/metrics/api.md#histogram-creation
+     */
     public function createHistogram(string $name, string $unit = null, string $description = null, array $advisory = []): HistogramInterface
     {
         return new HistogramAdapter($name, $description ?: '', $this->collectorRegistry);
     }
 
+    /**
+     * Creates an `ObservableGauge`.
+     *
+     * @param string $name name of the instrument
+     * @param string|null $unit unit of measure
+     * @param string|null $description description of the instrument
+     * @param array<string>|callable $advisory an optional set of recommendations, or
+     *        deprecated: the first callback to report measurements
+     * @param callable ...$callbacks responsible for reporting measurements
+     * @return ObservableGaugeInterface created instrument
+     *
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/metrics/api.md#asynchronous-gauge-creation
+     */
     public function createObservableGauge(string $name, string $unit = null, string $description = null, $advisory = [], callable ...$callbacks): ObservableGaugeInterface
     {
         throw new \LogicException(sprintf('Method %s is not implemented', __METHOD__));
     }
 
+    /**
+     * Creates an `UpDownCounter`.
+     *
+     * @param string $name name of the instrument
+     * @param string|null $unit unit of measure
+     * @param string|null $description description of the instrument
+     * @param array<string> $advisory an optional set of recommendations
+     * @return UpDownCounterInterface created instrument
+     *
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/metrics/api.md#updowncounter-creation
+     */
     public function createUpDownCounter(string $name, string $unit = null, string $description = null, array $advisory = []): UpDownCounterInterface
     {
         return new UpDownCounterAdapter($name, $description ?: '', $this->collectorRegistry);
     }
 
+    /**
+     * Creates an `ObservableUpDownCounter`.
+     *
+     * @param string $name name of the instrument
+     * @param string|null $unit unit of measure
+     * @param string|null $description description of the instrument
+     * @param array<string>|callable $advisory an optional set of recommendations, or
+     *        deprecated: the first callback to report measurements
+     * @param callable ...$callbacks responsible for reporting measurements
+     * @return ObservableUpDownCounterInterface created instrument
+     *
+     * @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/metrics/api.md#asynchronous-updowncounter-creation
+     */
     public function createObservableUpDownCounter(string $name, string $unit = null, string $description = null, $advisory = [], callable ...$callbacks): ObservableUpDownCounterInterface
     {
         throw new \LogicException(sprintf('Method %s is not implemented', __METHOD__));
