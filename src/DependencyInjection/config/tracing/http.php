869640fa3fac84b42513306f8f64ea5a2937f67a
Christian Daguerre <cdaguerre@worldia.com>
2025-01-10 09:36:43 +0100

    decorate http client when tracing is enabled

--- src/DependencyInjection/config/tracing/http.php
+++ src/DependencyInjection/config/tracing/http.php
@@ -7,13 +7,27 @@ declare(strict_types=1);
  * (c) Worldia <developers@worldia.com>
  */
 
+use Instrumentation\Http\TracingHttpClient;
 use Instrumentation\Tracing\Propagation\Http\TraceParentHeaderProvider;
 use Instrumentation\Tracing\Propagation\Http\TraceStateHeaderProvider;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $container) {
     $container->services()
         ->set(TraceParentHeaderProvider::class)
         ->set(TraceStateHeaderProvider::class)
     ;
+
+    if (class_exists(HttpClientInterface::class)) {
+        $container->services()
+            ->set(TracingHttpClient::class)
+            ->decorate(HttpClientInterface::class, null, 255)
+            ->args([
+                service('.inner'),
+            ])
+        ;
+    }
 };
