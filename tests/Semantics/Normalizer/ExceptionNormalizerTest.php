75653597237c566a3f69665be2bf67cb60628894
Camille Dejoye <camilledejoye@gmail.com>
2025-08-06 14:31:41 +0200

    feat: include previous exception information in normalized stacktrace
    PHP `\Throwable::getTraceAsString()` implementation only return the
    stack trace of the current exception object.
    It doesn't include any information from the previous expcetion(s).
    
    But `\Throwable::__toString()` does, it will show each exception type,
    message and stacktrace. From the oldest to the newest.
    
    This is valuable information when looking at the logs.
    Let's imagine we receive a request to sign in a new user, this request
    is validated and the validation fails because the password is too short.
    And that we wrap this error, later in the process, in a more generic
    error with the message "invalid credentials" error.
    Currently, all what the logs are gonna let us know is that the
    credentials were invalid.
    With this proposal, we will also be able to know that the password was
    too short.

--- tests/Semantics/Normalizer/ExceptionNormalizerTest.php
+++ tests/Semantics/Normalizer/ExceptionNormalizerTest.php
@@ -8,24 +8,86 @@
 namespace Tests\Instrumentation\Semantics\Normalizer;
 
 use Instrumentation\Semantics\Normalizer\ExceptionNormalizer;
+use OpenTelemetry\SemConv\TraceAttributes;
 use PHPUnit\Framework\TestCase;
 
 class ExceptionNormalizerTest extends TestCase
 {
     public function testNormalizesException(): void
     {
-        $exception = new \RuntimeException('Some exception');
+        $exception = new FixedStackTraceException(
+            'Something went wrong',
+            'Stack trace #10',
+        );
 
         $normalized = ExceptionNormalizer::normalizeException($exception);
 
-        $this->assertIsArray($normalized);
+        $this->assertArraysEqualIgnoringKeyOrder([
+            TraceAttributes::EXCEPTION_MESSAGE => 'Something went wrong',
+            TraceAttributes::EXCEPTION_TYPE => $exception::class,
+            TraceAttributes::EXCEPTION_STACKTRACE => <<<'EOL'
+            Test\Exception: Something went wrong
+            Stack trace #10
+            EOL,
+        ], $normalized);
+    }
 
-        $this->assertArrayHasKey('exception.message', $normalized);
-        $this->assertArrayHasKey('exception.type', $normalized);
-        $this->assertArrayHasKey('exception.stacktrace', $normalized);
+    public function testNormalizedExceptionContainsPreviousExceptionInformation(): void
+    {
+        $exception = new FixedStackTraceException(
+            'Something went wrong',
+            'Stack trace #10',
+            new FixedStackTraceException('Root cause', 'Stack trace #20'),
+        );
 
-        $this->assertIsString($normalized['exception.message']);
-        $this->assertIsString($normalized['exception.type']);
-        $this->assertIsString($normalized['exception.stacktrace']);
+        $normalized = ExceptionNormalizer::normalizeException($exception);
+
+        $this->assertArraysEqualIgnoringKeyOrder([
+            TraceAttributes::EXCEPTION_MESSAGE => 'Something went wrong',
+            TraceAttributes::EXCEPTION_TYPE => $exception::class,
+            TraceAttributes::EXCEPTION_STACKTRACE => <<<'EOL'
+            Test\Exception: Root cause
+            Stack trace #20
+            Next Test\Exception: Something went wrong
+            Stack trace #10
+            EOL,
+        ], $normalized);
+    }
+
+    /**
+     * @param array<string, string> $expected
+     * @param array<string, string> $actual
+     */
+    private function assertArraysEqualIgnoringKeyOrder(array $expected, array $actual): void
+    {
+        ksort($expected);
+        ksort($actual);
+        $this->assertEquals($expected, $actual);
+    }
+}
+
+final class FixedStackTraceException extends \Exception
+{
+    public function __construct(
+        string $message,
+        private string $stackTrace,
+        private self|null $previous = null,
+    ) {
+        parent::__construct($message, 0);
+    }
+
+    public function __toString()
+    {
+        return $this->previous?->format().$this->format();
+    }
+
+    private function format(): string
+    {
+        $message = 'Test\Exception: '.$this->message.\PHP_EOL.$this->stackTrace;
+        if ($this->previous) {
+            $message = \PHP_EOL.'Next '.$message;
+        }
+
+        return $message;
     }
 }
